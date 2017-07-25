64186ffd19744e8635bf829ae18dc318646a70f0
பாலாஜி <rbalajis25@gmail.com>
2017-07-25 14:06:49 +0530

    Fixes 1: sch00lb0y reference removed

--- plugin.js
+++ plugin.js
@@ -45,7 +45,7 @@ window.loklakFetcher = (function() {
       }
 
       // Create the URL with all the parameters
-      var url = 'http://35.188.41.86/api/search.json' +
+      var url = 'http://api.loklak.org/api/search.json' +
         '?callback=loklakFetcher.handleData' +
         '&q=' + query +
         '&count=' + options.count +
@@ -93,7 +93,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
   head.appendChild(link)
   }
-  addCss('https://sch00lb0y.github.io/loklak-timeline-plugin/style.css')
+  addCss('https://cdn.rawgit.com/fossasia/loklak-timeline-plugin/master/style.css')
   let elements = document.getElementsByClassName("loklak-timeline")
   for(let i = 0; i < elements.length; i++) {
     (function (element){
